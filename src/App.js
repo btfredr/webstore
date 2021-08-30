da714def9c4bb7fe4d61ccfedd5806012442a347
Fredrik <fredrikf2000@gmail.com>
2021-08-30 17:59:46 +0200

    started working on navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Navbar from "./components/Navbar/Navbar";
 import Products from "./components/Products/Products";
 
 const App = () => {
   return (
-    <div>
+    <>
+      <Navbar />
       <Products />
-    </div>
+    </>
   );
 };
 
